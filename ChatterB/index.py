5a9e28b418eb054aa4ca685446e3ef86d19743ed
Franck-F <franck-dilane1.fambou@epitech.digital>
2025-01-10 12:21:06 +0100

    *

--- ChatterB/index.py
+++ ChatterB/index.py
@@ -9,19 +9,36 @@
 from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
-bot = ChatBot("Jarvis", read_only=False, logic_adapters=["chatterbot.logic.BestMatch"])
+bot = ChatBot("Jarvis", read_only=False,
+              logic_adapters=[
+                  {
+                      "import_path": "chatterbot.logic.BestMatch",
+                      "default_response": "Sorry I don't have an answer",
+                      "maximum_similarity_threshold": 0.9
+                  }
+                  ])
 
-list_to_train = {
+list_to_train = [
     "hi",
     "hi there",
     "what's your name?",
     "I'm a chabot",
     "how old are you?",
-    "I'm ageless!"
-}
+    "I'm ageless!",
+    "why are you so sad?",
+    "i'm not!",
+    "Do you have iphone?",
+    "i've everything!",
+    "what's your favorite food?",
+    "I don't eat",
+    "what's your job",
+    "I'm here to answer your question",
+    "I don't know what you're talking about"
+]
 list_trainer = ListTrainer(bot)
 
 list_trainer.train()
 
-user_response = input("user: ")
-bot_response = printbot.get_response(user_response)
+while True:
+    user_response = input("user: ")
+    print("Jarvis: " + str(bot.get_response(user_response)))
